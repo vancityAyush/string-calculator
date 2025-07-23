6d77bae014cf6d28edde1cdb9c15fb8fd5c85637
vancityAyush <vancityAyush@gmail.com>
2025-07-24 01:18:21 +0530

    refactor: reorganized tests for clarity

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,117 +3,80 @@ import 'package:test/test.dart';
 
 void main() {
   group('StringCalculator', () {
-    late StringCalculator calculator;
+    late StringCalculator strCalc;
 
     setUp(() {
-      calculator = StringCalculator();
+      strCalc = StringCalculator();
     });
 
-    // Tests will be added incrementally through TDD cycles
-    // Starting with the most basic functionality
-
-    group('Basic Operations', () {
-      // Test cases will be added in subsequent TDD cycles
-
-      test('StringCalculator can be instantiated', () {
-        expect(calculator, isA<StringCalculator>());
-      });
-
-      test('empty string returns zero', () {
-        expect(calculator.add(''), equals(0));
-      });
-
-      test('single number returns value', () {
-        expect(calculator.add('1'), equals(1));
-      });
-
-      test('two comma separated numbers return sum', () {
-        expect(calculator.add('1,2'), equals(3));
-      });
-
-      test('multiple comma separated numbers return sum', () {
-        expect(calculator.add('1,2,3,4,5'), equals(15));
-      });
+    test('emptyReturnsZero', () {
+      expect(strCalc.add(''), equals(0));
     });
 
-    group('Delimiter Support', () {
-      // Test cases will be added in subsequent TDD cycles
-
-      test('newline delimiters work', () {
-        expect(calculator.add('1\n2'), equals(3));
-      });
-
-      test('mixed delimiters work', () {
-        expect(calculator.add('1\n,2,3\n4,5'), equals(15));
-      });
-
-      test('custom delimiters work', () {
-        expect(calculator.add('//;\n1;2'), equals(3));
-      });
+    test('singleReturnsValue', () {
+      expect(strCalc.add('1'), equals(1));
     });
 
-    group('Input Validation', () {
-      // Test cases will be added in subsequent TDD cycles
-
-      test('negative numbers throw exception', () {
-        expect(
-          () => calculator.add('-1'),
-          throwsA(isA<NegativeNumberException>()),
-        );
-      });
-
-      test('negative number exception contains correct message', () {
-        try {
-          calculator.add('-1');
-          fail('Expected NegativeNumberException to be thrown');
-        } catch (e) {
-          expect(e.toString(), contains('Negatives not allowed: -1'));
-        }
-      });
-
-      test('multiple negative numbers exception shows all negatives', () {
-        try {
-          calculator.add('-1,5,-10,-20');
-          fail('Expected NegativeNumberException to be thrown');
-        } catch (e) {
-          expect(e.toString(), equals('Negatives not allowed: -1, -10, -20'));
-        }
-      });
-
-      test('numbers greater than 1000 are ignored', () {
-        expect(calculator.add('2,1001,5'), equals(7));
-      });
+    test('twoCommaSeperatedReturnsSum', () {
+      expect(strCalc.add('1,2'), equals(3));
     });
 
-    group('Edge Cases', () {
-      // Test cases will be added in subsequent TDD cycles
+    test('nCommaSeperatedReturnsSum', () {
+      expect(strCalc.add('1,2,3,4,5'), equals(15));
+    });
 
-      test('any length delimiters work', () {
-        expect(calculator.add('//[***]\n1***2***3'), equals(6));
-      });
+    test('twoLineSeperatedReturnsSum', () {
+      expect(strCalc.add('1\n2'), equals(3));
+    });
 
-      test('single character bracket delimiter works', () {
-        expect(calculator.add('//[;]\n1;2;3'), equals(6));
-      });
+    test('nLineSeperatedReturnsSum', () {
+      expect(strCalc.add('1\n,2,3\n4,5'), equals(15));
+    });
 
-      test('very long delimiter works', () {
-        expect(
-          calculator.add('//[---DELIM---]\n1---DELIM---2---DELIM---3'),
-          equals(6),
-        );
-      });
+    test('twoAnyDelimeterSeperatedReturnsSum', () {
+      expect(strCalc.add('//;\n1;2'), equals(3));
+    });
 
-      test('multiple different delimiters work', () {
-        expect(calculator.add('//[*][%]\n1*2%3'), equals(6));
-      });
+    test('nAnyDelimeterSeperatedReturnsSum', () {
+      expect(strCalc.add('//;\n1;2;3;4;5'), equals(15));
+    });
 
-      test('multiple different delimiters with more complex case', () {
-        expect(calculator.add('//[*][%][!]\n1*2%3!4'), equals(10));
-      });
+    test('singleNegativeThrowsException', () {
+      expect(
+        () => strCalc.add('-1'),
+        throwsA(
+          isA<NegativeNumberException>().having(
+            (e) => e.toString(),
+            'message',
+            startsWith('Negatives not allowed: -1'),
+          ),
+        ),
+      );
+    });
 
-      test('multiple different delimiters with long delimiters', () {
-        expect(calculator.add('//[***][%%%]\n1***2%%%3'), equals(6));
-      });
+    test('nNegativesThrowsException', () {
+      expect(
+        () => strCalc.add('-1,5,-10,-20'),
+        throwsA(
+          isA<NegativeNumberException>().having(
+            (e) => e.toString(),
+            'message',
+            equals('Negatives not allowed: -1, -10, -20'),
+          ),
+        ),
+      );
+    });
+
+    test('ignoreGreaterThanThousand', () {
+      expect(strCalc.add('2,1001,5'), equals(7));
+    });
+
+    test('anyLengthDelimeters', () {
+      expect(strCalc.add('//[***]\n1***2***3'), equals(6));
+    });
+
+    test('multipleDelimeters', () {
+      expect(strCalc.add('//[*][%]\n1*2%3'), equals(6));
     });
   });
 }
