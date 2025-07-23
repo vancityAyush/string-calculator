8e40f7d377c85484c7b0170f9e9882cea38805c2
vancityAyush <vancityAyush@gmail.com>
2025-07-24 00:03:35 +0530

    test: add test for empty string returns zero

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,7 +2,38 @@ import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('StringCalculator', () {
+    late StringCalculator calculator;
+
+    setUp(() {
+      calculator = StringCalculator();
+    });
+
+    // Tests will be added incrementally through TDD cycles
+    // Starting with the most basic functionality
+    
+    group('Basic Operations', () {
+      // Test cases will be added in subsequent TDD cycles
+      
+      test('StringCalculator can be instantiated', () {
+        expect(calculator, isA<StringCalculator>());
+      });
+
+      test('empty string returns zero', () {
+        expect(calculator.add(''), equals(0));
+      });
+    });
+
+    group('Delimiter Support', () {
+      // Test cases will be added in subsequent TDD cycles
+    });
+
+    group('Input Validation', () {
+      // Test cases will be added in subsequent TDD cycles
+    });
+
+    group('Edge Cases', () {
+      // Test cases will be added in subsequent TDD cycles
+    });
   });
 }
